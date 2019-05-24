d88a894d008fa28196e9662722b296361571e8e2
Jonathan Bayless <baylessj@purdue.edu>
2019-05-24 10:12:19 -0400

    Initial draft of schedule

--- js/scripts.js
+++ js/scripts.js
@@ -175,6 +175,51 @@ $(document).ready(function () {
         $('#btn-show-content').toggleClass('toggle-map-content');
     });
 
+    /********************** Timeline ******************************/
+    $(function(){
+      window.sr = ScrollReveal();
+
+      if ($(window).width() < 768) {
+      	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+      	}
+
+      	sr.reveal('.js--fadeInRight', {
+    	    origin: 'right',
+    	    distance: '300px',
+    	    easing: 'ease-in-out',
+    	    duration: 800,
+    	  });
+      } else {
+      	sr.reveal('.js--fadeInLeft', {
+    	    origin: 'left',
+    	    distance: '300px',
+    		  easing: 'ease-in-out',
+    	    duration: 800,
+    	  });
+
+    	  sr.reveal('.js--fadeInRight', {
+    	    origin: 'right',
+    	    distance: '300px',
+    	    easing: 'ease-in-out',
+    	    duration: 800,
+    	  });
+      }
+      sr.reveal('.js--fadeInLeft', {
+    	    origin: 'left',
+    	    distance: '300px',
+    		  easing: 'ease-in-out',
+    	    duration: 800,
+    	  });
+
+    	  sr.reveal('.js--fadeInRight', {
+    	    origin: 'right',
+    	    distance: '300px',
+    	    easing: 'ease-in-out',
+    	    duration: 800,
+    	  });
+    });
+
     /********************** Add to Calendar **********************/
     var myCalendar = createCalendar({
         options: {
