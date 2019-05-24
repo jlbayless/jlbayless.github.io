d6ab3dc1812ffc6372130a83f2dda64053feae8b
Jonathan Bayless <baylessj@purdue.edu>
2019-05-24 13:21:46 -0400

    Add to calendar button working on schedule page

--- js/scripts.js
+++ js/scripts.js
@@ -229,23 +229,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Jonathan and Lillian's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jun 14, 2020 16:30'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jun 14, 2020 22:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '701 N Pennsylvania St, Indianapolis, IN 46204',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
+            description: "We can't wait to see you on our big day!"
         }
     });
 
