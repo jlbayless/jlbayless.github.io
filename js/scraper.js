109494aeebdd6c11f95bc3383253bfb143ade00b
Jonathan Bayless <baylessj@purdue.edu>
2019-06-01 17:27:20 -0400

    Synchronous read

--- js/scraper.js
+++ js/scraper.js
@@ -22,12 +22,8 @@ puppeteer
     const page = await browser.newPage();
     console.log('Waiting for page to load');
 
-    return page.goto(url, {waitUntil: 'networkidle0'}).then(function() {
-      page
-        .waitForSelector('.wedding-product-tile') // probably extraneous because of the 'waitUntil'
-        .then(() => console.log('Container visible'));
-      return page.content();
-    });
+    return page.goto(url, {waitUntil: 'networkidle0'})
+    .then(function() { return page.content(); });
   })
   .then(async html => {
     const $ = await cheerio.load(html);
@@ -39,32 +35,24 @@ puppeteer
       });
     })
 
-    var idx = 0;
-    var arr = [];
-    var image = '';
-    fs.readFileSync(file, 'utf8', function(err, data) {
-      if (err) throw err;
-      arr = data.toString().split('\n');
+    var fileContents = fs.readFileSync(file, 'utf8');
+    var arr = fileContents.toString().split('\n');
+    console.log(arr);
+    var selector = 'id="registry-container"';
+
+    items.forEach(function(value) {
+      var image = '<img class="amazon-item-img" src="';
+      image += value.title;
+      image += '"></img>';
+
+      var replace = 0;
+      var prevImage = arr[idx + 1];
+      // console.log(prevImage);
+      if (prevImage != undefined && prevImage.includes('<img class="amazon-item-img"')) {
+        replace = 1;
+      }
+      arr.splice(idx, replace, image);
       // console.log(arr);
-      var selector = 'id="registry-container"';
-      // idx = arr.findIndex(function(val) {
-      //   return val.includes(selector);
-      // });
-
-      items.forEach(function(value) {
-        image = '<img class="amazon-item-img" src="';
-        image += value.title;
-        image += '"></img>';
-
-        var replace = 0;
-        var prevImage = arr[idx + 1];
-        // console.log(prevImage);
-        if (prevImage != undefined && prevImage.includes('<img class="amazon-item-img"')) {
-          replace = 1;
-        }
-        arr.splice(idx, replace, image);
-        console.log(arr);
-      });
     });
 // fs.writeFile(file, arr, (err) => if (err) throw err;);
   });
